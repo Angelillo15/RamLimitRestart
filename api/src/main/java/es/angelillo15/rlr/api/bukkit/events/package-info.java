b74878972718ca0923166652b1b0e72218b2e5b4
Angelillo15 <angelrizosrubios@gmail.com>
2022-10-15 12:54:15 +0200

    Change package-info.java

--- api/src/main/java/es/angelillo15/rlr/api/bukkit/events/package-info.java
+++ api/src/main/java/es/angelillo15/rlr/api/bukkit/events/package-info.java
@@ -1,5 +1,7 @@
 /**
- * here you can find all the events that are fired by the plugin
+ * Here you can find all the events that are fired by the plugin
+ * If you need to listen to any of these events, you can do it by using the Bukkit API
+ *
  * @author Angelillo15
  * @since 1.0
  * @version 1.0
